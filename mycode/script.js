19b5cc4560f1fdc7cda3a0066e25ab17672e3ef8
dandy1ion <104789558+dandy1ion@users.noreply.github.com>
2023-02-01 20:53:10 -0700

    Add files via upload

--- mycode/script.js
+++ mycode/script.js
@@ -348,7 +348,7 @@ Here are your tasks:
   
   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the DEFAULT OPTION. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
 HINT: Use many of the tools you learned about in this and the last section.
@@ -367,7 +367,7 @@ const poll = {
   //This generates [0, 0, 0, 0]. More in the next section
   answers: new Array(4).fill(0),
   registerNewAnswer() {
-    //Recieve Answer
+    //Receive Answer
     const answer = Number(
       prompt(
         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
